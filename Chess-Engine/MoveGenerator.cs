8c5b1fd09758d6e24edcc883f2f912a553244b93
echomachst8745 <joel.crann22@hotmail.co.uk>
2024-11-02 01:48:16 +0000

    added move generator class

---
+++ Chess-Engine/MoveGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Engine
+{
+    public struct Move
+    {
+        public int startSquareIndex;
+        public int endSquareIndex;
+    }
+
+    public static class MoveGenerator
+    {
+
+    }
+}
